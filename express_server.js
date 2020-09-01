a2ad70c86f332a583136f55159bd232d89b93e5b
doge33 <sandylx33@gmail.com>
2020-09-01 03:46:39 +0000

    installed nodemon; added DELETE buttons in form element;added route to handle delete & redirect back

--- express_server.js
+++ express_server.js
@@ -61,6 +61,14 @@ app.post("/urls", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req.params)
+  delete urlDatabase[req.params.shortURL]; //shortURL exists in req.params; undefined by itself
+  res.redirect("/urls/");
+
 })
 
 app.listen(PORT, () => {
