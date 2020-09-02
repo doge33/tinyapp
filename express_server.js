01ecc8eca3069f6b3fb1a5aee21fcdfb4beb4285
doge33 <sandylx33@gmail.com>
2020-09-02 02:10:24 +0000

    created global users variable object; added registration(post) handler which generates random newUserId

--- express_server.js
+++ express_server.js
@@ -9,18 +9,29 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 //generate 6 random alphanumeric characters
 function generateRandomString() {
   return Math.random().toString(36).substring(2,8);
 
 }
 
-
 //middlewares(process in btween req & resp)
 app.set("view engine", "ejs");  //for GET (so far)l
 app.use(bodyParser.urlencoded({extended:true}));  //for POST (to make data human-readable)
 app.use(cookieParser());
-
 
 // handlers
 app.get("/", (req, res) => {
@@ -70,37 +81,34 @@ app.get("/register", (req, res) => {
   res.render("register", templateVars)
 })
 
+
+//POST req starts HERE
 app.post("/urls", (req, res) => {
 
   const newURL = generateRandomString();
   urlDatabase[newURL] = req.body.longURL; // => this, req.body is where you utilize bodyParser!
    
   //console.log(urlDatabase); //log the POST req body to the console (for reference here)
-  res.redirect(`/urls/${newURL}`);     //after posting, it's better to redirect
+  res.redirect(`/urls/${newURL}`);     
 
 });
 
-
 app.post("/urls/:shortURL/delete", (req, res) => {
-  //console.log(req.params)
   delete urlDatabase[req.params.shortURL]; //shortURL exists in req.params; undefined by itself
   res.redirect("/urls/");
 
 });
 
 app.post("/urls/:id", (req, res) => {
-
   //console.log("req.params: " + req.params);
   //console.log("req.body.longURL: " + req.body.longURL);
   //console.log("urlDatabase[req.params.id]: "+ urlDatabase[req.params.id]);
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls");
-  
 });
 
 app.post("/login", (req, res) => {
   //console.log(`(value of)the username in req.body: ${req.body.username}`);
-
   res.cookie("username", req.body.username); // the username and its value is in the body of the POST req sent by client. Set it in server's response
   res.redirect("/urls");
 });
@@ -112,6 +120,18 @@ app.post("/logout", (req, res) => {
 
 });
 
+app.post("/register", (req, res) => {
+
+  const newUserId = generateRandomString();
+
+  users[newUserId] = {
+    id: newUserId,
+    email: req.body.email,
+    password: req.body.password,  
+  }
+  res.cookie("user_id", newUserId);
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
